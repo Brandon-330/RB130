ccdbe7e55e66b25f87f1ec9ba6c0c0f557ed8523
Brandon Lima <112784850+Brandon-330@users.noreply.github.com>
2023-06-14 02:54:26 +0000

    new file:   exercises/easy1/exercise1.rb 	new file:   exercises/easy1/exercise2.rb 	new file:   exercises/easy1/exercise3.rb 	new file:   exercises/easy1/exercise4.rb 	new file:   exercises/easy1/exercise5.rb

---
+++ exercises/easy1/exercise1.rb
@@ -0,0 +1,3 @@
+class Tree
+  module Enumerable
+end
---
+++ exercises/easy1/exercise5.rb
@@ -0,0 +1,38 @@
+def rot13(string)
+  ALPHABET_POSITION = Hash.new
+
+  counter = 0
+  ('a..z').each do |letter|
+    ALPHABET_POSITION[counter] = letter
+    counter += 1
+  end
+
+  rot13.downcase.chars.map do |char|
+    if char =~ /[a-z]/
+      place_in_alphabet = ALPHABET_POSITION[char]
+      place_in_alphabet += 13
+      ALPHABET_POSITION
+    else
+      char
+  end
+end
+
+Nqn Ybirynpr
+Tenpr Ubccre
+Nqryr Tbyqfgvar
+Nyna Ghevat
+Puneyrf Onoontr
+Noqhyynu Zhunzznq ova Zhfn ny-Xujnevmzv
+Wbua Ngnanfbss
+Ybvf Unvog
+Pynhqr Funaaba
+Fgrir Wbof
+Ovyy Tngrf
+Gvz Orearef-Yrr
+Fgrir Jbmavnx
+Xbaenq Mhfr
+Fve Nagbal Ubner
+Zneiva Zvafxl
+Lhxvuveb Zngfhzbgb
+Unllvz Fybavzfxv
+Tregehqr Oynapu
