2ae021e5dde0efbe93b40db6975a82af25076cf0
Brandon Lima <112784850+Brandon-330@users.noreply.github.com>
2023-06-21 22:33:27 +0000

    new file:   cat.rb 	modified:   exercises/medium1/not_finished_exercise6.rb 	modified:   test.rb

--- test.rb
+++ test.rb
@@ -1,7 +1,19 @@
 require 'minitest/autorun'
 
-class MyFirstTest < Minitest::Test
-  def test_first_test
-    assert true
+require_relative 'cat'
+
+class CatTest < Minitest::Test
+  def setup
+    @kitty = Cat.new('Kitty')
+  end
+
+  def test_name
+    assert_match(/a\./, 'a.bc')
+  end
+
+  def test_no_arg
+    assert_raises do
+      Cat.new('ok')
+    end
   end
 end
