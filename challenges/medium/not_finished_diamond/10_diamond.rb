abf4238cac731ff83f37c30f9eeee1a53b37b3ca
Brandon Lima <112784850+Brandon-330@users.noreply.github.com>
2023-07-07 22:38:11 +0000

    modified:   challenges/medium/not_finished_clock/clock.rb 	modified:   challenges/medium/not_finished_diamond/10_diamond.rb

--- challenges/medium/not_finished_diamond/10_diamond.rb
+++ challenges/medium/not_finished_diamond/10_diamond.rb
@@ -1,40 +1,62 @@
+=begin
+5:35
+
+### Problem
+# Input: String (a letter)
+# Output: Diamond print
+
+## Rules
+# Explicit:
+  - Given a letter, start the letter with 'A' and go down to the middle
+  - From the middle, go back up to letter 'A'
+  - The diamond is horizontally and vertically symmetric
+
+# Implicit:
+  - No integers, floats, or collections as input
+
+### Data Structure
+# Formulate an alhabet array, which includes alphabets up to and including given letter
+  - Add everything except last letter to this array
+# The alphabet array will be the diamond from top to bottom
+# Line number will be considered the alphabet array's index
+# First and last line are different and happens once
+# Depending on the line number, space will be different
+
+## Methods
+#each
+#size
+#each_with_object
+#puts
+=end
+
 class Diamond
-  @@letters = []
-
-  def self.make_diamond(end_letter)
-    track_letters(end_letter)
-
-    total_lines = ((@@letters.size * 2) - 1)
-    idx = -1
-    total_lines.times do |line_number|
-      idx += 1
-      
-      if line_number > total_lines / 2
-        idx -= 2
-      end
-
-      print_diamond(idx)
+  def self.make_diamond(given_letter)
+    alphabet_array = ('A'..given_letter).each_with_object([]) do |char, array|
+      array << char
     end
+    ('A'...given_letter).reverse_each { |char| alphabet_array << char } unless given_letter == 'A'
+
+    print_diamond(alphabet_array)
   end
 
-  def self.track_letters(end_letter)
-    ('A'..'Z').each do |alphabet|
-      if (end_letter <=> alphabet) >= 0
-        @@letters << alphabet
+  def self.print_diamond(alphabet_array)
+    middle_line = (alphabet_array.size + 1) / 2
+    outer_space = middle_line - 1
+    return_diamond = ''
+
+    alphabet_array.each_with_index do |letter, idx|
+      inner_space = alphabet_array.size - 2 - (outer_space * 2)
+
+      return_diamond += "#{' ' * outer_space}#{letter}"
+      return_diamond += "#{' ' * inner_space}#{letter}" unless letter == 'A'
+      return_diamond += "#{' ' * outer_space}\n"
+      if idx + 1 < middle_line
+        outer_space -= 1
+      else
+        outer_space += 1
       end
     end
-  end
 
-  def self.print_diamond(idx)
-    size = @@letters.size - 1
-    first_spaces = size - idx
-    secondary_spaces = idx
-    secondary_spaces += 1 if idx > 1
-    
-    print ' ' * first_spaces + @@letters[idx] + ' ' * secondary_spaces
-    print @@letters[idx] unless @@letters[idx] == 'A'
-    puts ' ' * first_spaces
+    return_diamond
   end
 end
-
-Diamond.make_diamond('A') == 'A'
