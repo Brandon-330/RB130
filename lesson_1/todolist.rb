dcc4df47e6816ab877ae61b8a1b2b75722369b18
Brandon Lima <112784850+Brandon-330@users.noreply.github.com>
2023-07-14 23:01:08 +0000

    modified:   lesson_1/todolist.rb 	modified:   lesson_2/todolist_test.rb

--- lesson_1/todolist.rb
+++ lesson_1/todolist.rb
@@ -1,118 +1,6 @@
-class TodoList
-  attr_accessor :title, :todos
-
-  def initialize(title)
-    @title = title
-    @todos = []
-  end
-
-  def add(todo)
-    if todo.is_a? Todo
-      todos << todo
-    else
-      raise TypeError, "Can only add Todo objects"
-    end
-  end
-
-  def size
-    todos.size
-  end
-
-  def first
-    todos.first
-  end
-
-  def last
-    todos.last
-  end
-
-  def to_a
-    todos
-  end
-
-  def done?
-    todos.all? { |todo| todo.done }
-  end
-
-  def item_at(index)
-    todos.fetch(index)
-  end
-
-  def mark_done_at(index)
-    item_at(index).done!
-  end
-
-  def mark_undone_at(index)
-    item_at(index).undone!
-  end
-
-  def done!
-    todos.each { |todo| todo.done! }
-  end
-
-  def shift
-    todos.shift
-  end
-
-  def pop
-    todos.pop
-  end
-
-  def remove_at(index)
-    todos.delete_at(index)
-  end
-
-  def each
-    @todos.each do |todo|
-      yield(todo)
-    end
-
-    self
-  end
-
-  def select
-    new_todolist = TodoList.new(title)
-    each do |todo|
-      new_todolist.add(todo) if yield(todo)
-    end
-
-    new_todolist
-  end
-
-  def find_by_title(title_string)
-    new_arr = []
-    each { |todo| new_arr << todo if todo.title == title_string }
-
-    new_arr.first
-  end
-
-  def all_done
-    select { |todo| todo.done? }
-  end
-
-  def all_not_done
-    select { |todo| !todo.done? }
-  end
-
-  def mark_done(title_string)
-    find_by_title(title_string) && find_by_title(title_string).done!
-  end
-
-  def mark_all_done
-    each { |todo| todo.done! }
-  end
-
-  def mark_all_undone
-    each { |todo| todo.undone! }
-  end
-
-  def to_s
-    <<~block
-     ---- Today's Todos ----
-     #{todos.each { |todo| puts todo }}
-    block
-  end
-end
+# This class represents a todo item and its associated
+# data: name and description. There's also a "done"
+# flag to show whether this todo item is done.
 
 class Todo
   DONE_MARKER = 'X'
@@ -149,19 +37,118 @@ class Todo
   end
 end
 
+# This class represents a collection of Todo objects.
+# You can perform typical collection-oriented actions
+# on a TodoList object, including iteration and selection.
 
-todo1 = Todo.new("Buy milk")
-todo2 = Todo.new("Clean room")
-todo3 = Todo.new("Go to gym")
+class TodoList
+  attr_accessor :title
 
-list = TodoList.new("Today's Todos")
-list.add(todo1)
-list.add(todo2)
-list.add(todo3)
+  def initialize(title)
+    @title = title
+    @todos = []
+  end
 
-todo1.done!
+  def size
+    @todos.size
+  end
 
-# puts list.find_by_title('Buy milk')
-# puts list.all_not_done
-# puts list.mark_done('Clean room')
-# puts list.todos
+  def first
+    @todos.first
+  end
+
+  def last
+    @todos.last
+  end
+
+  def shift
+    @todos.shift
+  end
+
+  def pop
+    @todos.pop
+  end
+
+  def done?
+    @todos.all? { |todo| todo.done? }
+  end
+
+  def <<(todo)
+    raise TypeError, 'can only add Todo objects' unless todo.instance_of? Todo
+
+    @todos << todo
+  end
+  alias_method :add, :<<
+
+  def item_at(idx)
+    @todos.fetch(idx)
+  end
+
+  def mark_done_at(idx)
+    item_at(idx).done!
+  end
+
+  def mark_undone_at(idx)
+    item_at(idx).undone!
+  end
+
+  def done!
+    @todos.each_index do |idx|
+      mark_done_at(idx)
+    end
+  end
+
+  def remove_at(idx)
+    @todos.delete(item_at(idx))
+  end
+
+  def to_s
+    text = "---- #{title} ----\n"
+    text << @todos.map(&:to_s).join("\n")
+    text
+  end
+
+  def to_a
+    @todos.clone
+  end
+
+  def each
+    @todos.each do |todo|
+      yield(todo)
+    end
+    self
+  end
+
+  def select
+    list = TodoList.new(title)
+    each do |todo|
+      list.add(todo) if yield(todo)
+    end
+    list
+  end
+
+  # returns first Todo by title, or nil if no match
+  def find_by_title(title)
+    select { |todo| todo.title == title }.first
+  end
+
+  def all_done
+    select { |todo| todo.done? }
+  end
+
+  def all_not_done
+    select { |todo| !todo.done? }
+  end
+
+  def mark_done(title)
+    find_by_title(title) && find_by_title(title).done!
+  end
+
+  def mark_all_done
+    each { |todo| todo.done! }
+  end
+
+  def mark_all_undone
+    each { |todo| todo.undone! }
+  end
+end
--- lesson_2/todolist_test.rb
+++ lesson_2/todolist_test.rb
@@ -2,12 +2,10 @@ require 'minitest/autorun'
 require "minitest/reporters"
 Minitest::Reporters.use!
 
-require 'simplecov'
-SimpleCov.start
-
 require_relative 'todolist'
 
 class TodoListTest < MiniTest::Test
+
   def setup
     @todo1 = Todo.new("Buy milk")
     @todo2 = Todo.new("Clean room")
@@ -21,139 +19,44 @@ class TodoListTest < MiniTest::Test
   end
 
   def test_to_a
-    assert_equal(@todos, @list.to_a)
+    assert_equal @todos, @list.to_a
   end
 
   def test_size
-    assert_equal(3, @list.size)
+    assert_equal 3, @list.size
   end
 
   def test_first
-    assert_equal(@todo1, @list.first)
+    assert_equal @todo1, @list.first
   end
 
   def test_last
-    assert_equal(@todo3, @list.last)
+    assert_equal @todo3, @list.last
   end
 
   def test_shift
-    assert_equal(@todo1, @list.shift) 
-    assert_equal([@todo2, @todo3], @list.to_a)
+    assert_equal @todo1, @list.shift
+    assert_equal 2, @list.size
   end
 
-  def test_pop
-    assert_equal(@todo3, @list.pop)
-    assert_equal([@todo1, @todo2], @list.to_a)
+  def test_pop 
+    assert_equal @todo3, @list.pop
+    assert_equal 2, @list.size
   end
 
   def test_done?
-    assert_equal(false, @list.done?)
-  end
-
-  def test_added_not_todo_obj
-    assert_raises(TypeError) { @list.add(3) }
-    assert_raises(TypeError) { @list.add('hi')}
-  end
-
-  def test_append
-    new_todo = Todo.new('Homework')
-    @list << new_todo
-    @todos << new_todo
-    assert_equal(@todos, @list.to_a)
-  end
-
-  def test_add
-    new_todo = Todo.new('Homework')
-    @list.add(new_todo)
-    @todos << new_todo
-    assert_equal(@todos, @list.to_a)
-  end
-
-  def test_item_at
-    assert_raises(IndexError) { @list.item_at(3) }
-    assert_raises(IndexError) { @list.item_at(-4) }
-    assert_equal(@todo1, @list.item_at(0))
-  end
-
-  def test_mark_done_at
-    assert_raises(IndexError) { @list.mark_done_at(3) }
-    assert_raises(IndexError) { @list.mark_done_at(-4) }
-    assert_equal(true, @list.mark_done_at(0))
-    assert_equal(@todo1.done?, true)
-    assert_equal(@todo2.done?, false)
-  end
-
-  def test_mark_undone_at
-    assert_raises(IndexError) { @list.mark_done_at(3) }
-    assert_raises(IndexError) { @list.mark_done_at(-4) }
-    @list.mark_done_at(1)
-    assert_equal(false, @list.mark_undone_at(1))
-  end
-
-  def test_done!
+    assert_equal false, @list.done?
     @list.done!
-    assert_equal(true, @todo1.done?)
-    assert_equal(true, @todo2.done?)
-    assert_equal(true, @todo3.done?)
-    assert_equal(true, @list.done?)
+    assert_equal true, @list.done?
   end
 
-  def test_remove_at
-    assert_raises(IndexError) { @list.mark_done_at(3) }
-    assert_raises(IndexError) { @list.mark_done_at(-4) }
-    assert_equal(@todo1, @list.remove_at(0))
-    assert_equal([@todo2, @todo3], @list.to_a)
+  def test_adding_not_todo
+    assert_raise ArgumentError do
+      @list.add 'Hello'
+    end
   end
-
-  def test_to_s
-    output = <<~OUTPUT.chomp
-    ---- Today's Todos ----
-    [ ] Buy milk
-    [ ] Clean room
-    [ ] Go to gym
-    OUTPUT
   
-    assert_equal(output, @list.to_s)
-  end
-
-  def test_to_s_some_done
-    @todo2.done!
-    @todo3.done!
-
-    output = <<~OUTPUT.chomp
-    ---- Today's Todos ----
-    [ ] Buy milk
-    [X] Clean room
-    [X] Go to gym
-    OUTPUT
-
-    assert_equal(output, @list.to_s)
-  end
-
-  def test_to_s_all_done
-    @list.done!
-
-    output = <<~OUTPUT.chomp
-    ---- Today's Todos ----
-    [X] Buy milk
-    [X] Clean room
-    [X] Go to gym
-    OUTPUT
-
-    assert_equal(output, @list.to_s)
-  end
-
-  def test_each
-    assert_equal(@list, @list.each { |todo| todo.done! })
-    assert_equal(true, @list.done?)
-  end
-
-  def test_select
-    @todo1.done!
-    list = TodoList.new(@list.title)
-    list.add(@todo1)
-  
-    assert_equal(list.title, @list.title)
-    assert_equal(list.to_s, @list.select{ |todo| todo.done? }.to_s)
+  def test_<<
+    
   end
 end
